dd0c37ae4427ee36520c7d6c1bd3a999660c63bf
scaprio89 <scaprio@daymarksi.com>
2016-11-15 15:59:42 -0500

    Add files via upload

---
+++ Bulk User Creation/NewUserPS.ps1
@@ -0,0 +1,1 @@
+﻿Import-Csv -Path "C:\My Documents\NewAccounts.csv" | foreach {New-MsolUser -DisplayName $_.DisplayName -FirstName $_.FirstName -LastName $_.LastName -UserPrincipalName $_.UserPrincipalName -UsageLocation $_.UsageLocation -LicenseAssignment $_.AccountSkuId} | Export-Csv -Path "C:\My Documents\NewAccountResults.csv"
